999154036b660b79584501fd236c453f56e5bd3c
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2018-12-16 14:03:15 +0000

    Small fix to check if there is a notebooks folder when pulling a repo in pull_src_notebooks

--- lib/build.py
+++ lib/build.py
@@ -38,20 +38,26 @@ def pull_src_notebooks(pkgs=PKGS_YML, tgt_folder=NBS_FOLDER,
             cmd = (f'git clone https://github.com/pysal/{subpackage}.git '\
                    f'{tmp}/dls/{subpackage}')
             os.system(cmd)
-            nbs = [i for i in os.listdir(f'{tmp}/dls/{subpackage}/notebooks') \
-                   if i[-6:]=='.ipynb']
-            if len(nbs) > 0:
-                os.system(f'mkdir -p {tgt_folder}/{package}/{subpackage}/')
-                for i in range(len(nbs)):
-                    src_nb = nbs[i].replace(' ', '\ ')
-                    cmd = (f'mv {tmp}/dls/{subpackage}/notebooks/{src_nb} '\
-                           f'{tgt_folder}/{package}/{subpackage}/{src_nb}')
-                    os.system(cmd)
-            else:
+            nbf_exists = True
+            try:
+                os.listdir(f'{tmp}/dls/{subpackage}/notebooks')
+            except:
+                nbf_exists = False
                 print((f"\nWARNING: {package}/{subpackage} does NOT"\
-                        " contain notebooks\n"))
-            break
-        break
+                        " contain a notebook folder\n"))
+            if nbf_exists:
+                nbs = [i for i in os.listdir(f'{tmp}/dls/{subpackage}/notebooks') \
+                       if i[-6:]=='.ipynb']
+                if len(nbs) > 0:
+                    os.system(f'mkdir -p {tgt_folder}/{package}/{subpackage}/')
+                    for i in range(len(nbs)):
+                        src_nb = nbs[i].replace(' ', '\ ')
+                        cmd = (f'mv {tmp}/dls/{subpackage}/notebooks/{src_nb} '\
+                               f'{tgt_folder}/{package}/{subpackage}/{src_nb}')
+                        os.system(cmd)
+                else:
+                    print((f"\nWARNING: {package}/{subpackage} does NOT"\
+                            " contain notebooks\n"))
     os.system(f'rm -rf {tmp}')
     t1 = time.time()
     print(f"\n\t{t1-t0} seconds to collect notebooks")
@@ -67,7 +73,7 @@ def setup_book(bk_folder=BOOK_FOLDER, template_folder=TEMPLATE_FOLDER):
     os.system(f'rm -r {BOOK_FOLDER}/content')
     os.system(f'cp -r {NBS_FOLDER} {BOOK_FOLDER}/content')
     os.system(f'cp -r {TEMPLATE_FOLDER}/content/intro.md {BOOK_FOLDER}/content')
-    os.system(f'python {BOOK_FOLDER}/scripts/generate_book.py')
+    os.system(f'cd {BOOK_FOLDER} && make book')
     return None
 
 if __name__ == '__main__':
